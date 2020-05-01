4f4ba4bfcd7d2e9416c39ff903fdd6a89e3e86da
Ruben <ruben.soler@hotmail.es>
2020-05-01 11:34:28 +0200

    Init app

---
+++ app/src/main/java/soler/ruben/rickandmorty/MainActivity.kt
@@ -0,0 +1,12 @@
+package soler.ruben.rickandmorty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
